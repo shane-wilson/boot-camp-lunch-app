885922a87c89eae01a301046c53b9385e6ae5ff4
shane.wilson <shane.wilson@digital.gmrc.gov.uk>
2017-10-11 12:09:30 +0100

    Added extra welcome tests

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -25,5 +25,12 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
       val result = controller.welcome.apply(FakeRequest())
       contentType(result) mustBe Some("text/html")
     }
+
+    "say hello and have a title" in {
+      val controller = new WelcomeController
+      val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+      contentAsString(result) must include ("<h1>Hello!</h1>")
+      contentAsString(result) must include ("<title>Welcome!</title>")
+    }
   }
 }
